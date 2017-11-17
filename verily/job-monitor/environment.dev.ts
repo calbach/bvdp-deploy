834cdbf16c774acf521e144b700cce40b7127558
Bryan Crampton <bryancrampton@google.com>
2017-11-17 12:47:56 -0800

    [Job-Monitor] Change scope cloud-platform -> cloudplatformprojects (#11)

--- verily/job-monitor/environment.dev.ts
+++ verily/job-monitor/environment.dev.ts
@@ -6,7 +6,7 @@ export const environment = {
   clientId: '488108584931-jcdqb1s30gc8us84ksv62i10ncp9rrpb.apps.googleusercontent.com',
   production: true,
   requiresAuth: true,
-  scope: 'https://www.googleapis.com/auth/genomics https://www.googleapis.com/auth/cloud-platform.read-only',
+  scope: 'https://www.googleapis.com/auth/genomics https://www.googleapis.com/auth/cloudplatformprojects.readonly',
   additionalColumns: dsubAdditionalColumns,
   entryPoint: 'projects',
 };
