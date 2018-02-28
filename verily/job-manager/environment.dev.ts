45a66e59352702af67f7567f5d95bce48bf396b8
Bryan Crampton <bryancrampton@google.com>
2018-02-27 18:21:09 -0800

    [Job-Manager] Update to latest version with capabilities (#18)

--- verily/job-manager/environment.dev.ts
+++ verily/job-manager/environment.dev.ts
@@ -3,9 +3,4 @@ export const environment = {
   // From GCP project bvdp-verily-dev
   clientId: '488108584931-jcdqb1s30gc8us84ksv62i10ncp9rrpb.apps.googleusercontent.com',
   production: true,
-  requiresAuth: true,
-  scope: 'email https://www.googleapis.com/auth/genomics https://www.googleapis.com/auth/cloudplatformprojects.readonly',
-  additionalColumns: ['user-id', 'job-id', 'task-id'],
-  entryPoint: 'projects',
-  serverUrl: '',
 };
